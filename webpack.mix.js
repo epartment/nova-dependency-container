d4ad8962010e084a488bb8a5454a75b25d779446
Michiel Kempen <kempenmichiel@gmail.com>
2018-09-15 15:56:23 +0200

    build assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,6 @@
 let mix = require('laravel-mix')
 
 mix.js('resources/js/field.js', 'dist/js')
-   .sass('resources/sass/field.scss', 'dist/css')
     .webpackConfig({
         resolve: {
             symlinks: false
