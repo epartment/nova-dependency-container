2e7019aeedb62e6762c0681456df382cb908b030
Matt Ivie <mattivie14@gmail.com>
2018-12-11 16:27:34 -0700

    Update NovaDependencyContainer.php
    Added 'dependsOnFalse' function and section in 'resolveForDisplay' supporting that function

--- src/NovaDependencyContainer.php
+++ src/NovaDependencyContainer.php
@@ -47,6 +47,20 @@ class NovaDependencyContainer extends Field
             'dependencies' => array_merge($this->meta['dependencies'], [['field' => $field, 'value' => $value]])
         ]);
     }
+    
+    /**
+     * Adds a dependency for a field being NOT equal to a value
+     *
+     * @param $field
+     * @param $value
+     * @return $this
+     */
+    public function dependsOnFalse($field, $value)
+    {
+        return $this->withMeta([
+            'dependencies' => array_merge($this->meta['dependencies'], [['field' => $field, 'falseValue' => $value]])
+        ]);
+    }
 
     /**
      *
@@ -75,6 +89,10 @@ class NovaDependencyContainer extends Field
             }
 
             if(array_key_exists('value', $dependency) && $dependency['value'] == $resource->{$dependency['field']}) {
+                $this->meta['dependencies'][$index]['satisfied'] = true;
+            }
+            
+            if(array_key_exists('falseValue', $dependency) && $dependency['falseValue'] !== $resource->{$dependency['field']}) {
                 $this->meta['dependencies'][$index]['satisfied'] = true;
             }
         }
