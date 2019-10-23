43936b7cd26d1c10cf45f5e65803526e74ee16bf
wize-wiz <9093559+wize-wiz@users.noreply.github.com>
2019-10-23 20:48:39 +0200

    Refactor watcher, add callback when watchers are set, fix for #88

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -22,8 +22,9 @@
 		props: ['resourceName', 'resourceId', 'field'],
 
 		mounted() {
-			this.registerDependencyWatchers(this.$root)
-			this.updateDependencyStatus()
+			this.registerDependencyWatchers(this.$root, function() {
+				this.updateDependencyStatus();
+			});
 		},
 
 		data() {
@@ -35,27 +36,45 @@
 
 		methods: {
 
-			registerDependencyWatchers(root) {
+			// @todo: refactor entire watcher procedure, this approach isn't maintainable ..
+			registerDependencyWatchers(root, callback) {
+				callback = callback || null;
 				root.$children.forEach(component => {
 					if (this.componentIsDependency(component)) {
 
-						let attribute = this.findWatchableComponentAttribute(component);
+						// @todo: change `findWatchableComponentAttribute` to return initial state(s) of current dependency.
+						let attribute = this.findWatchableComponentAttribute(component),
+							initial_value = component.field.value; // @note: quick-fix for issue #88
+
 						component.$watch(attribute, (value) => {
+							// @todo: move to reactive factory
 							if (attribute === 'selectedResource') {
 								value = (value && value.value) || null;
 							}
 							this.dependencyValues[component.field.attribute] = value;
+							// @todo: changed value as argument for `updateDependencyStatus`
 							this.updateDependencyStatus()
-						}, {immediate: true})
+						}, {immediate: true});
 
-						this.dependencyValues[component.field.attribute] = component.field.value;
+						// @todo: move to initial state
+						// @note quick-fix for issue #88
+						if (attribute === 'fieldTypeName') {
+							initial_value = component.field.resourceLabel;
+						}
 
+						// @todo: replace with `updateDependencyStatus(initial_value)` and let it resolve dependency state
+						this.dependencyValues[component.field.attribute] = initial_value;
 					}
 
 					this.registerDependencyWatchers(component)
-				})
+				});
+
+				if(callback !== null) {
+					callback.call(this);
+				}
 			},
 
+			// @todo: not maintainable, move to factory
 			findWatchableComponentAttribute(component) {
 				let attribute;
 				switch(component.field.component) {
@@ -85,6 +104,7 @@
 				return false;
 			},
 
+			// @todo: align this method with the responsibility of updating the dependency, not verifying the dependency "values"
 			updateDependencyStatus() {
 				for (let dependency of this.field.dependencies) {
 
