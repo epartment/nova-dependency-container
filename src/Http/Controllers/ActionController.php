019c0cef27f94190b76acb764c1ad1496a96401a
David Cox <dave.cox@dunntire.com>
2021-03-02 19:43:01 +0000

    fix spacing

--- src/Http/Controllers/ActionController.php
+++ src/Http/Controllers/ActionController.php
@@ -8,17 +8,17 @@ use Laravel\Nova\Http\Controllers\ActionController as NovaActionController;
 
 class ActionController extends NovaActionController
 {
-	/**
+    /**
      * This uses the custom ActionRequest typehint to enable
      * dependency features.
-	 *
-	 * @param  \Laravel\Nova\Http\Requests\ActionRequest  $request
-	 * @return \Illuminate\Http\Response
-	 */
+     *
+     * @param  \Laravel\Nova\Http\Requests\ActionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(NovaActionRequest $request)
     {
         $request = ActionRequest::createFrom($request);
 
         return parent::store($request);
-	}
+    }
 }
