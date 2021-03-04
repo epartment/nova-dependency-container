089764fbf7ef03a0d14d5f6fcce92a630039d09f
David Cox <dave.cox@dunntire.com>
2021-03-04 18:55:02 +0000

    update action field validation for latest compatible nova code

--- src/ActionHasDependencies.php
+++ src/ActionHasDependencies.php
@@ -3,18 +3,13 @@
 namespace Epartment\NovaDependencyContainer;
 
 use Laravel\Nova\Http\Requests\ActionRequest;
+use Illuminate\Support\Facades\Validator;
 
 trait ActionHasDependencies
 {
     use HasChildFields;
 
-    /**
-     * Validate action fields
-     *
-     * @param  \Laravel\Nova\Http\Requests\ActionRequest  $request
-     * @return void
-     */
-    public function validateFields(ActionRequest $request)
+    protected function fieldsForValidation(ActionRequest $request)
     {
         $availableFields = [];
 
@@ -33,9 +28,33 @@ trait ActionHasDependencies
         if ($this->childFieldsArr) {
             $availableFields = array_merge($availableFields, $this->childFieldsArr);
         }
+    }
 
-        $this->validate(collect($availableFields)->mapWithKeys(function ($field) {
-            return $field->getCreationRules($this);
-        })->all());
+    /**
+     * Validate action fields. Mostly a copy paste from Nova
+     *
+     * Uses the above to validate only on fields that have satisfied dependencies.
+     *
+     * @param  \Laravel\Nova\Http\Requests\ActionRequest  $request
+     * @return void
+     */
+    public function validateFields(ActionRequest $request)
+    {
+        $fields = collect($this->fieldsForValidation($request));
+
+        return Validator::make(
+            $request->all(),
+            $fields->mapWithKeys(function ($field) use ($request) {
+                return $field->getCreationRules($request);
+            })->all(),
+            [],
+            $fields->reject(function ($field) {
+                return empty($field->name);
+            })->mapWithKeys(function ($field) {
+                return [$field->attribute => $field->name];
+            })->all()
+        )->after(function ($validator) use ($request) {
+            $this->afterValidation($request, $validator);
+        })->validate();
     }
 }
